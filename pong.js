89f4c59fd70a2ad85466e131da048bddf4d9fd62
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-29 14:13:27 +0200

    draw rect function

--- pong.js
+++ pong.js
@@ -1,3 +1,11 @@
 // select canvas
 const cvs = document.getElementById("pong");
 const ctx = cvs.getContext("2d");
+
+// draw rect function
+function drawRect(x,y,w,h,color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x,y,w,h);
+}
+
+drawRect(0,0,cvs.width, cvs.height, "BLACK")
