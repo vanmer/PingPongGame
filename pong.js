8a706791a6e91dbf3bb9e9065c14caa95257e582
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-29 14:18:16 +0200

    draw circle function

--- pong.js
+++ pong.js
@@ -3,9 +3,20 @@ const cvs = document.getElementById("pong");
 const ctx = cvs.getContext("2d");
 
 // draw rect function
-function drawRect(x,y,w,h,color) {
+function drawRect(x, y, w, h, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x,y,w,h);
+  ctx.fillRect(x, y, w, h);
 }
 
-drawRect(0,0,cvs.width, cvs.height, "BLACK")
+drawRect(0,0,cvs.width, cvs.height, "BLACK");
+
+// draw circle function
+function drawCircle(x, y, r, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, false);
+  ctx.closePath();
+  ctx.fill();
+}
+
+drawCircle(100, 100, 10, "WHITE");
